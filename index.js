72b34c4d03b8119e9a67c646a4a23d7bbfab1f51
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-09 14:36:49 -0700

    updated function 'sum' to include recursion

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+
+  // this 'if' statement is the base case
+  if (fromN === toN) {
+    return fromN;
+  }
+
+  // this is the 'recursive case'. It runs until fromN === toN (as indicated in the base case above)
+  return fromN + sum(fromN + 1, toN);
+
 }
+
+
+// console.log(sum(3, 7));
+// console.log(sum(1, 1))
 
 module.exports = sum;
